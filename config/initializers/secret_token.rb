f8ec0270354a7c5bf2c00dcd169b5c3ac2842132
kanechika <kanechika7@gmail.com>
2011-12-25 17:04:14 +0900

    cs_api start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CsApi::Application.config.secret_token = '3963015619da25323c443759fdff451d00b9ca9bfe32136f4a3b3938b43efb0167fb631a2519715fe8ced68b07017373764fe545be78d528ba9644d42e63a0f9'
