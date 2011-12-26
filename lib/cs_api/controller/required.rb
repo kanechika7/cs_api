d87e6c8fbe322daa7bf35cf6fee8eab66a714f73
kanechika <kanechika7@gmail.com>
2011-12-26 15:32:00 +0900

    required

--- lib/cs_api/controller/required.rb
+++ lib/cs_api/controller/required.rb
@@ -14,12 +14,24 @@ module CsApi
         # @author Nozomu Kanechika
         # @from 0.0.1
         def cs_required
+          return render json: 'cookie session is not exists.' if session['user_info'].nil?
         end
 
         # cookie role よりアクセス権限があるか
         # @author Nozomu Kanechika
         # @from 0.0.1
         def cs_role_required
+          r = JSON.parse(session['user_info'])['roles'].detect{|r| r=~/^#{params[:controller]}-/ }
+          return render json: 'your role is not permitted.' if r.nil?
+          rs = r.split('-')
+          case params[:action]
+          when 'index','show'
+            return render json: 'your role is not permitted.' unless rs[1]=~/^r/
+          when 'create','update','copy','destroy'
+            return render json: 'your role is not permitted.' unless rs[1]=='rw'
+          else
+            return render json: 'your role is not permitted.'
+          end
         end
 
       end
